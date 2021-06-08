42ce6fd1d2a88bfc9124aa5ddfee63f54ed69359
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-06-08 14:53:04 +0300

    [clients][portable-windows-cmd] Integrated go client. Refactoring in scripts.

--- 0-download-env.bat
+++ 0-download-env.bat
@@ -12,7 +12,7 @@ cd %ROOT%
 rd /S /Q %GIT_HOME%
 mkdir %GIT_HOME%
 powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%ARCH_GIT%','%TOOLS%\git.zip') }"
-%TOOLS%\7z x -y -o%GIT_HOME% %TOOLS%\git.zip
+%ARCH% x -y -o%GIT_HOME% %TOOLS%\git.zip
 cd %ROOT%
 
 echo off
@@ -26,10 +26,8 @@ echo on
 cd %ROOT%
 rd /S /Q %JAVA_HOME%
 powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, 'oraclelicense=accept-securebackup-cookie'); $client.DownloadFile('%ARCH_JDK%','%TOOLS%\jdk.zip') }"
-%TOOLS%\7z x -y -o%ROOT% %TOOLS%\jdk.zip
+%ARCH% x -y -o%TOOLS%\.. %TOOLS%\jdk.zip
 rename %ARCH_JDK_FOLDER% .jdk
-cd %ROOT%
-
 cd %ROOT%
 
 echo off
--- 00-settings.bat
+++ 00-settings.bat
@@ -3,43 +3,49 @@ set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=0000000
 set LANGUAGE=java-script
 
 set ROOT=%CD%
-set TOOLS=%ROOT%\.tools
-set JAVA_HOME=%ROOT%\.jdk
-set GIT_HOME=%ROOT%\.gitTool
-set MAVEN_HOME=%ROOT%\.maven
-set GIT=%GIT_HOME%\cmd\git
 
 set GIT_REPO=https://github.com/codenjoyme/codenjoy.git
 set GIT_REVISION=master
 set REBUILD_SOURCES=ask
-
 set APP_HOME=%ROOT%\.server-distr
 set CODENJOY_HOME=%ROOT%\.codenjoy
 set SERVER_SOURCES=%CODENJOY_HOME%\CodingDojo\server
 set GAMES_SOURCES=%CODENJOY_HOME%\CodingDojo\games
 set CLIENT_SOURCES=%CODENJOY_HOME%\CodingDojo\clients
-set MVNW=%CODENJOY_HOME%\CodingDojo\mvnw
 set CODENJOY_VERSION=1.1.1
-
 set GAMES_TO_RUN=ask
 set CONTEXT=/codenjoy-contest
 set PORT=8080
 set SPRING_PROFILES=sqlite
 set DEBUG=false
+set SKIP_TESTS=true
+set CODE_PAGE=65001
 
-set JAVA_CLIENT_HOME=%CODENJOY_HOME%\CodingDojo\clients\java
-set PSEUDO_CLIENT_HOME=%CODENJOY_HOME%\CodingDojo\clients\pseudo
+set TOOLS=%ROOT%\.tools
+set ARCH=%TOOLS%\7z
+set JAVA_HOME=%ROOT%\.jdk
+set MAVEN_HOME=%ROOT%\.maven
+set MAVEN_OPTS=-Dmaven.repo.local=%MAVEN_HOME%\repository
+set MVNW=%CODENJOY_HOME%\CodingDojo\mvnw
+set NODE_HOME=%ROOT%\.node
+set NODE=%NODE_HOME%\node
+set NPM=%NODE_HOME%\npm
+set GOPATH=%ROOT%\.golang\go
+set GO=%GOPATH%\bin\go
+set GIT_HOME=%ROOT%\.gitTool
+set GIT=%GIT_HOME%\cmd\git
+
+set JAVA_CLIENT_HOME=%CLIENT_SOURCES%\java
+set PSEUDO_CLIENT_HOME=%CLIENT_SOURCES%\pseudo
 set PSEUDO_RULES=%PSEUDO_CLIENT_HOME%\rules
 set PSEUDO_HERO_ELEMENTS=BOMBERMAN,BOMB_BOMBERMAN,DEAD_BOMBERMAN
+set JAVASCRIPT_CLIENT_HOME=%CLIENT_SOURCES%\java-script
+set GO_CLIENT_HOME=%CLIENT_SOURCES%\go
 
 set ARCH_GIT=https://github.com/git-for-windows/git/releases/download/v2.18.0.windows.1/MinGit-2.18.0-64-bit.zip
-
-set ARCH_NODE=https://nodejs.org/dist/v14.17.0/node-v14.17.0-win-x64.zip
-set ARCH_NODE_FOLDER=node-v14.17.0-win-x64
-
 set ARCH_JDK=https://aka.ms/download-jdk/microsoft-jdk-11.0.11.9.1-windows-x64.zip
 set ARCH_JDK_FOLDER=jdk-11.0.11+9
-
-set SKIP_TESTS=true
-set MAVEN_OPTS=-Dmaven.repo.local=%MAVEN_HOME%\repository
-set CODE_PAGE=65001
+set ARCH_NODE=https://nodejs.org/dist/v14.17.0/node-v14.17.0-win-x64.zip
+set ARCH_NODE_FOLDER=node-v14.17.0-win-x64
+set ARCH_GO=https://golang.org/dl/go1.16.5.windows-amd64.zip
+set ARCH_GO_FOLDER=go
